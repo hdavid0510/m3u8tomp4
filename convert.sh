e0e15018d42ba0ab0d02acb4a92b7456b1fb325b
David Hong <gdavid0510@gmail.com>
2020-05-05 14:09:02 +0900

    Handle file name with spaces

--- convert.sh
+++ convert.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-LISTDIR="/mnt/d/m3m8/input"
-TARGDIR="/mnt/d/m3m8/output"
+LISTDIR="$PWD/m3u8"
+DESTDIR="$PWD/m3u8"
 EXT="m3u8"
 THREAD=3
 
-for file in $(find $LISTDIR -type f -name "*.$EXT"); do
-	((i=i%THREAD)); ((i++==0)) && wait
-	echo "Processing: $file"
-	ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i file://$file -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 $TARGDIR/$(echo $file | sed -e 's/'$(echo $LISTDIR | sed -e 's/\//\\\//g')'//g' -e 's/m3u8/mp4/g') > $TARGDIR/$(echo $file | sed -e 's/'$(echo $LISTDIR | sed -e 's/\//\\\//g')'//g' -e 's/m3u8/log/g' ) 2>&1 &
+find "$LISTDIR" -type f -name "*.$EXT" -print0 | while read -d '' file
+do
+    ((i=i%THREAD)); ((i++==0)) && wait
+	echo -e "\e[34mProcessing: $file\e[0m"
+	ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto \
+		-i "file://$file" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 \
+		"$DESTDIR"$(echo "${file//$LISTDIR/}" | sed -e 's/\.m3u8/\.mp4/g')  \
+		> "$DESTDIR"$(echo "${file//$LISTDIR/}" | sed -e 's/.m3u8/.log/g' ) 2>&1 &
 done
