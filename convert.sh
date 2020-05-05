ced96bf8ad79ea66381845638143e056ef3414ce
David Hong <gdavid0510@gmail.com>
2020-05-06 05:08:11 +0900

    multithreading problem fixed

--- convert.sh
+++ convert.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 LISTDIR="$PWD/m3u8"
-DESTDIR="$PWD/m3u8"
+DESTDIR="$PWD"
 EXT="m3u8"
-THREAD=3
+MAX_THREAD=4
+
+download(){
+	# $1 = origin file
+	# $2 = destination file
+	echo -e "\e[93mBegin converting\e[0m\n\e[93m   Input file=\e[0m $1\n\e[93m  Output file=\e[0m $2\n\n"
+	ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -nostdin\
+		-i "file://$1" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 30 \
+		"$2" > "$2".log 2>&1
+}
 
 find "$LISTDIR" -type f -name "*.$EXT" -print0 | while read -d '' file
 do
-    ((i=i%THREAD)); ((i++==0)) && wait
-	echo -e "\e[34mProcessing: $file\e[0m"
-	ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto \
-		-i "file://$file" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 \
-		"$DESTDIR"$(echo "${file//$LISTDIR/}" | sed -e 's/\.m3u8/\.mp4/g')  \
-		> "$DESTDIR"$(echo "${file//$LISTDIR/}" | sed -e 's/.m3u8/.log/g' ) 2>&1 &
+    ((i=i%MAX_THREAD)); ((i++==0)) && wait
+	download "$file" "$DESTDIR$(echo ${file//$LISTDIR/} | sed -E 's|(.*)\.'$EXT'|\1.mp4|')" &
 done
